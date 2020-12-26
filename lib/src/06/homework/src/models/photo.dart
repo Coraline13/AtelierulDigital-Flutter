52aef87d7510cee3d8374f1b5ff437cc17322ece
Coraline <coralia.bodea@gmail.com>
2020-12-26 21:43:54 +0200

    Add unsplash_api

--- lib/src/06/homework/src/models/photo.dart
+++ lib/src/06/homework/src/models/photo.dart
@@ -1,5 +1,6 @@
 library photo;
 
+import 'package:atelieruldigital_flutter/src/06/homework/src/models/serializers.dart';
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
@@ -14,6 +15,19 @@ abstract class Photo implements Built<Photo, PhotoBuilder> {
   }
 
   Photo._();
+
+  int get id;
+
+  int get width;
+
+  int get height;
+
+  String get description;
+
+  @BuiltValueField(wireName: 'alt_description')
+  String get altDescription;
+
+  Map<String, String> get urls;
 
   static Serializer<Photo> get serializer => _$photoSerializer;
 }
