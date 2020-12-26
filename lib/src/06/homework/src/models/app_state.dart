ee3885c062b7a8e6186a78bb1723ae0d4d4cf152
Coraline <coralia.bodea@gmail.com>
2020-12-26 20:13:03 +0200

    Generate models

---
+++ lib/src/06/homework/src/models/app_state.dart
@@ -0,0 +1,28 @@
+library app_state;
+
+import 'package:atelieruldigital_flutter/src/06/homework/src/models/photo.dart';
+import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
+import 'package:built_value/serializer.dart';
+
+part 'app_state.g.dart';
+
+abstract class AppState implements Built<AppState, AppStateBuilder> {
+  factory AppState([void Function(AppStateBuilder b) updates]) = _$AppState;
+
+  factory AppState.initialState() {
+    final AppStateBuilder builder = AppStateBuilder();
+    builder
+      ..isLoading = true
+      ..nextPage = 1;
+    return builder.build();
+  }
+
+  AppState._();
+
+  BuiltList<Photo> get photos;
+
+  int get nextPage;
+
+  bool get isLoading;
+}
